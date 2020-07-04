b8981cae395d011db47ec11db63c85f249af81b3
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-07-03 21:40:10 -0500

    Adicion de campos

--- lib/scr/pages/InputPage.dart
+++ lib/scr/pages/InputPage.dart
@@ -6,8 +6,108 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  final colorIconos = Colors.purple;
+
+  String _nombre = '';
+  String _email = '';
+
+  int _tamanoNombre = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Input de Texto'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+        children: <Widget>[
+          _crearInput(),
+          Divider(),
+          _crearCorreo(),
+          Divider(),
+          _crearPassword(),
+          Divider(),
+          _crearPersona(),
+        ],
+      ),
+    );
+  }
+
+  Widget _crearInput() {
+    return TextField(
+      cursorColor: Colors.red,
+      enabled: true,
+      textCapitalization: TextCapitalization.words,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+        counter: Text('# Letras $_tamanoNombre'),
+        hintText: 'Nombre de la persona',
+        labelText: 'Nombre',
+        helperText: 'Sólo es el nombre',
+        suffixIcon: Icon(
+          Icons.accessibility,
+          color: colorIconos,
+        ),
+        prefixIcon: Icon(Icons.input, color: colorIconos),
+        icon: Icon(Icons.account_circle, color: colorIconos),
+      ),
+      onTap: () {
+        print('Pedro onTap');
+      },
+      onChanged: (valor) {
+        setState(() {
+          _nombre = valor;
+          _tamanoNombre = _nombre.length;
+        });
+      },
+    );
+  }
+
+  Widget _crearCorreo() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      cursorColor: Colors.purple,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+        hintText: 'Email',
+        labelText: 'Email',
+        suffixIcon: Icon(
+          Icons.alternate_email,
+          color: colorIconos,
+        ),
+        icon: Icon(Icons.email, color: colorIconos),
+      ),
+      onChanged: (valor) => setState(() {
+        _email = valor;
+      }),
+    );
+  }
+
+  Widget _crearPassword() {
+    return TextField(
+      obscureText: true,
+      cursorColor: Colors.purple,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+        hintText: 'Password',
+        labelText: 'Password',
+        suffixIcon: Icon(
+          Icons.lock,
+          color: colorIconos,
+        ),
+        icon: Icon(Icons.lock_outline, color: colorIconos),
+      ),
+      onChanged: (valor) => setState(() {
+        _email = valor;
+      }),
+    );
+  }
+
+  Widget _crearPersona() {
+    return ListTile(
+      subtitle: Text('${_email.length == 0 ? '' : 'El email es $_email'}'),
+      title: Text('${_nombre.length == 0 ? '' : 'El nombre es $_nombre'}'),
+    );
   }
 }
