40cd519781f6b03c6c9ffea5ab5111030f74f908
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-07-05 18:48:04 -0500

    Crear Slider Page 3

--- lib/scr/pages/slider_page3.dart
+++ lib/scr/pages/slider_page3.dart
@@ -7,8 +7,6 @@ class SliderPage3 extends StatefulWidget {
 
 class _SliderPageState extends State<SliderPage3> {
   double _valorDelSlider = 200.0;
-  double _valorDelSlider2 = 200.0;
-  bool _bloquearSlider = false;
 
   @override
   Widget build(BuildContext context) {
@@ -58,51 +56,6 @@ class _SliderPageState extends State<SliderPage3> {
               _valorDelSlider = valor;
             });
           }),
-    );
-  }
-
-  Widget _crearImagen() {
-    return FadeInImage(
-        fadeInDuration: Duration(milliseconds: 200),
-        placeholder: AssetImage('assets/jar-loading.gif'),
-        width: _valorDelSlider,
-        fit: BoxFit.contain,
-        image: NetworkImage(
-            'https://enfilme.com/img/content/aef34739ab2cbfda14fc7a35fb012d3a38_675_489.jpg'));
-  }
-
-  Widget _crearCheckBox() {
-//    return Checkbox(
-//        activeColor: Colors.red,
-//        value: _bloquearSlider,
-//        onChanged: (valor) {
-//          setState(() {
-//            _bloquearSlider = valor;
-//          });
-//        });
-
-    return CheckboxListTile(
-        title: Text('Bloquer Slider'),
-        value: _bloquearSlider,
-        onChanged: (valor) {
-          setState(() {
-            _bloquearSlider = valor;
-          });
-        });
-  }
-
-  Widget _crearSlider2() {
-    return Slider(
-      activeColor: Colors.purple,
-      label: 'aaaaaaaaa',
-      value: _valorDelSlider2,
-      onChanged: (value) {
-        setState(() {
-          _valorDelSlider2 = value;
-        });
-      },
-      max: 500.0,
-      min: 50.0,
     );
   }
 }
